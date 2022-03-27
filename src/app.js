de5654dca93003cbb78db8bcbef7048b8131fb38
unknown <musalordkai@gmail.com>
2022-03-27 11:41:51 -0700

    Adds github pages deployment

--- src/app.js
+++ src/app.js
@@ -95,6 +95,13 @@ createTask: async () => {
   App.setLoading(true)
   const content = $('#newTask').val()
   await App.todoList.createTask(content)
+  window.location.reload(  )
+},
+
+toggleCompleted: async (e) => {
+  App.setLoading(true)
+  const taskId = e.target.name
+  await App.todoList.toggleCompleted(taskId)
   window.location.reload()
 },
 
